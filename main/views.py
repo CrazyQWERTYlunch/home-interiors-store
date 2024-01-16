d813b6adcadd94203a0e39c297ad8ce97a7d446d
UzNik <uznik.99@mail.ru>
2024-01-16 12:58:47 +0300

    First simple example

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+# Create your views here.
+def index(request): #  в request Попадают данные о запросе и пользователе
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - Home'
+    }
+    return render(request, 'main/index.html', context=context)
+
+def about(request) -> HttpResponse: #  в request Попадают данные о запросе и пользователе
+    return HttpResponse('About us')
