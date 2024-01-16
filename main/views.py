0141313e2bd89e78e6ccd79b98fc9b550f0c8be2
UzNik <uznik.99@mail.ru>
2024-01-16 15:05:16 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -5,11 +5,16 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request): #  в request Попадают данные о запросе и пользователе
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context=context)
 
 
-def about(request) -> HttpResponse: #  в request Попадают данные о запросе и пользователе
-    return HttpResponse('About us')
+def about(request): #  в request Попадают данные о запросе и пользователе
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему это классный магазин'
+    }
+    return render(request, 'main/about.html', context=context)
