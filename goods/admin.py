31b18d03f6fd1014e87e65b28d481ffbc9daab35
UzNik <uznik.99@mail.ru>
2024-01-17 17:55:57 +0300

    After adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Products
+
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+# Модель подключения таблиц в админ-панели с более тонкой настройкой (автозаполнение поля) 
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+from goods.models import Categories
 
 # Create your views here.
 def index(request): #  в request Попадают данные о запросе и пользователе
+    
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME'
+        'content': 'Магазин мебели HOME',
+        'categories': categories
     }
     return render(request, 'main/index.html', context=context)
 
